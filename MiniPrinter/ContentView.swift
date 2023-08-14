98437cbac638b247efed0f7777ab4ab9e6232ed0
pxy <panxinyu@mgtv.com>
2023-08-14 09:09:19 +0800

    Initial Commit

---
+++ MiniPrinter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MiniPrinter
+//
+//  Created by 潘新宇 on 2023/8/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniPrinter/MiniPrinterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniPrinterApp.swift
+//  MiniPrinter
+//
+//  Created by 潘新宇 on 2023/8/14.
+//
+
+import SwiftUI
+
+@main
+struct MiniPrinterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
